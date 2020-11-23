305d7b3db03eb32eb99a290e62f02a46c7917b55
Priyavrat Misra <PriyavratMisra123@gmail.com>
2020-11-23 22:51:58 +0530

    fine-tune pretrained resnet-18

--- custom_dataset.py
+++ custom_dataset.py
@@ -9,12 +9,13 @@ class ChestXRayDataset(torch.utils.data.Dataset):
         def get_images(class_name):
             images = [x for x in os.listdir(
                 image_dirs[class_name]) if x[-3:].lower().endswith('png')]
+            print(f'found {len(images)} {class_name} examples.')
             return images
 
         self.images = {}
-        self.class_names = ['normal', 'viral', 'covid']
+        self.classes = ['covid', 'normal', 'viral']
 
-        for class_name in self.class_names:
+        for class_name in self.classes:
             self.images[class_name] = get_images(class_name)
 
         self.image_dirs = image_dirs
@@ -22,13 +23,13 @@ class ChestXRayDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return sum(
-            [len(self.images[class_name]) for class_name in self.class_names]
+            [len(self.images[class_name]) for class_name in self.classes]
         )
 
     def __getitem__(self, index):
-        class_name = random.choice(self.class_names)
+        class_name = random.choice(self.classes)
         index = index % len(self.images[class_name])
         image_name = self.images[class_name][index]
         image_path = os.path.join(self.image_dirs[class_name], image_name)
         image = Image.open(image_path).convert('RGB')
-        return self.transform(image), self.class_names.index(class_name)
+        return self.transform(image), self.classes.index(class_name)
--- preprocess_data.py
+++ preprocess_data.py
@@ -19,8 +19,15 @@ if os.path.isdir(os.path.join(root_dir, source_dirs[1])):
     for c in class_names:
         images = [x for x in os.listdir(os.path.join(
             root_dir, c)) if x.lower().endswith('png')]
-        selected_images = random.sample(images, 30)
+        selected_images = random.sample(images, 40)
         for image in selected_images:
             source_path = os.path.join(root_dir, c, image)
             target_path = os.path.join(root_dir, 'test', c, image)
             shutil.move(source_path, target_path)
+
+    os.mkdir(os.path.join(root_dir, 'train'))
+
+    for c in class_names:
+        source_path = os.path.join(root_dir, c)
+        target_path = os.path.join(root_dir, 'train')
+        shutil.move(source_path, target_path)
